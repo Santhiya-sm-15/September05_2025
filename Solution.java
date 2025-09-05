ca926832bf309c35aefbb3cf0032bffb021fc67a
Santhiya <169180434+Santhiya-sm-15@users.noreply.github.com>
2025-09-05 09:22:45 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,73 @@
+class Solution {
+    public Node segregate(Node head) {
+        Node zero=null,z=null;
+        Node one=null,o=null;
+        Node two=null,t=null;
+        for(Node i=head;i!=null;)
+        {
+            Node nxt=i.next;
+            if(i.data==0)
+            {
+                if(zero==null)
+                {
+                    zero=i;
+                    z=i;
+                    z.next=null;
+                }
+                else
+                {
+                    z.next=i;
+                    z=i;
+                }
+            }
+            else if(i.data==1)
+            {
+                if(one==null)
+                {
+                    one=i;
+                    o=i;
+                    o.next=null;
+                }
+                else
+                {
+                    o.next=i;
+                    o=i;
+                }
+            }
+            else
+            {
+                if(two==null)
+                {
+                    two=i;
+                    t=i;
+                    t.next=null;
+                }
+                else
+                {
+                    t.next=i;
+                    t=i;
+                }
+            }
+            i.next=null;
+            i=nxt;
+        }
+        head=null;
+        if(zero!=null)
+        {
+            head=zero;
+            z.next=one;
+            if(one!=null)
+                o.next=two;
+            else
+                z.next=two;
+        }
+        else if(one!=null)
+        {
+            head=one;
+            o.next=two;
+        }
+        else
+            head=two;
+        return head;
+    }
+}
